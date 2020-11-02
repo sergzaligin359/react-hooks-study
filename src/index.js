2f05feaef246550deebd9a992e5e94064535becb
sergzaligin359 <webdevjsphp@gmail.com>
2020-11-02 13:03:33 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-import Routes from 'routes'
-import Topbar from 'components/topbar'
+import Routes from 'routes';
+import { Topbar, CurrentUserChecker } from 'components';
+
+import { CurrentUserProvider } from 'context/currentUser';
 
 const App = () => {
   return (
-    <Router>
-      <Topbar />
-      <Routes />
-    </Router>
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <Topbar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
   )
 }
 
